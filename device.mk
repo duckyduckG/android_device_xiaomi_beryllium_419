97070171c8d33f0b1478b8033c619ca0c6168671
duckyduckG <duckyduckg65@gmail.com>
2026-02-04 16:57:29 +0500

    beryllium: copy cgroups_30.json and task_profiles_30.json
    make schedtune optional to fix following issue:
    
    [   13.001986][    T1] cgroup1: Unknown subsys name 'schedtune'
    [   13.007738][    T1] libprocessgroup: Failed to mount controller schedtune: Invalid argument
    [   13.016210][    T1] libprocessgroup: Failed to setup schedtune cgroup
    [   13.022760][    T1] init: Command 'SetupCgroups' action=SetupCgroups (<Builtin Action>:0) took 41ms and failed: Failed to setup cgroups: Invalid argument
    
    Change-Id: I27847493ec2119f65e63fe948196223e8a91c3a8

--- device.mk
+++ device.mk
@@ -50,8 +50,8 @@ PRODUCT_COPY_FILES += \
 
 # Control groups and task profiles
 PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Device fstab
 PRODUCT_PACKAGES += \
