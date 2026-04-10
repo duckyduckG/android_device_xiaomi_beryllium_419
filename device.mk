04fda43c96af515bf9b5a1c3a4d23024c98ffbe1
duckyduckG <duckyduckg65@gmail.com>
2026-04-10 23:32:08 +0500

    beryllium: move qdcm files to device tree
    Change-Id: I12e53475f6c1adc0b1f69fbaa28eaf20f86d55bf

--- device.mk
+++ device.mk
@@ -84,6 +84,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     XiaomiPocketMode
 
+# QDCM
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/qdcm/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
